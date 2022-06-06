5b34e883841e9fd9ea68370e8600dfe21b672ee8
Burak CELEN <burakcelen@starkajans.com>
2022-06-06 16:00:35 +0300

    Initial commit

---
+++ assets/script/script.js
@@ -0,0 +1,36 @@
+var emailError = document.getElementById('email-error');
+var passwordError = document.getElementById('password-error');
+
+function validateMail() {
+    var email = document.getElementById('form-email').value;
+    if(!email.match(/^[A-Za-z\._\-[0-9]*[@][A-Za-z]*[\.][a-z]{2,4}$/)){
+        emailError.innerHTML = 'Email Invalid'
+        return false;
+    }
+    if(email == "") {
+        emailError.innerHTML = ''
+    }
+    emailError.innerHTML = '<i class="fas fa-check-circle"></i>'
+}
+function validatePassword() {
+      var password = document.getElementById('form-password').value;
+    if(!password.match(/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/)){
+        passwordError.innerHTML = 'Password Invalid'
+        return false;
+    }
+    passwordError.innerHTML = '<i class="fas fa-check-circle"></i>'
+}
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    spaceBetween: 50,
+    autoplay: {
+        delay: 5000,
+        disableOnInteraction: false,
+        pauseOnMouseEnter: true,
+      },
+    loop: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
